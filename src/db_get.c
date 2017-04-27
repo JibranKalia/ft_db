37a544f60850fc9405f03064e3d3c346f6c547da
Jibran Kalia <jibran.kalia@gmail.com>
2017-04-26 20:45:06 -0700

    Simplified db_get slightly

--- src/db_get.c
+++ src/db_get.c
@@ -6,7 +6,7 @@
 /*   By: aakin-al <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/26 02:57:48 by aakin-al          #+#    #+#             */
-/*   Updated: 2017/04/26 20:12:52 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/26 20:44:43 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,17 +90,14 @@ int			db_get(t_server *server)
 	CHK1(server->flag_tbl_load == false, db_msg(server, MSG_TBL_MISSING), 0);
 	CHK1(server->argc != 2, db_reply(server, "usage: GET key\n"), 0);
 	if (strncasecmp(server->args[1], "all", 3) == 0)
-		db_getall(server);
-	else
-	{
-		filename = ft_strjoinf("/", db_gethash(server, server->args[1]), STRJOIN_FREE_SRC2);
-		filename = ft_strjoinf(server->tblpath, filename, STRJOIN_FREE_SRC2);
-		CHK2(((fp = fopen(filename, "r")) == NULL), free(filename), perror("FOPEN ERROR"), -1);
-		fd = fileno(fp);
-		CHK2(fstat(fd, &st) == -1, free(filename), perror("FSTAT ERROR"), -1);
-		db_get_print(server, fp, st.st_size * 2);
-		CHK2(fclose(fp) == EOF, free(filename), perror("FCLOSE ERROR"), -1);
-		free(filename);
-	}
+		return(db_getall(server));
+	filename = ft_strjoinf("/", db_gethash(server, server->args[1]), STRJOIN_FREE_SRC2);
+	filename = ft_strjoinf(server->tblpath, filename, STRJOIN_FREE_SRC2);
+	CHK2(((fp = fopen(filename, "r")) == NULL), free(filename), perror("FOPEN ERROR"), -1);
+	fd = fileno(fp);
+	CHK2(fstat(fd, &st) == -1, free(filename), perror("FSTAT ERROR"), -1);
+	db_get_print(server, fp, st.st_size * 2);
+	CHK2(fclose(fp) == EOF, free(filename), perror("FCLOSE ERROR"), -1);
+	free(filename);
 	return (0);
 }
