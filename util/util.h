f97685e9238aa86ba3f99555a732d0b612e321cf
Jibran Kalia <jkalia@student.42.us.org>
2017-05-05 17:13:18 -0700

    More norming

--- util/util.h
+++ util/util.h
@@ -6,20 +6,23 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 16:35:21 by jkalia            #+#    #+#             */
-/*   Updated: 2017/05/05 16:37:58 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/05/05 17:13:03 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ft_db.h>
-#include <time.h>
+#ifndef UTIL_H
+# define UTIL_H
+# include <ft_db.h>
+# include <time.h>
 
 typedef struct sockaddr_in	t_sockaddr_in;
 typedef struct hostent		t_hostent;
 
-typedef struct				t_db_client
+typedef struct				s_db_client
 {
 	int						portno;
 	int						sockfd;
 	t_sockaddr_in			serv_addr;
 	t_hostent				*server;
 }							t_db_client;
+#endif
